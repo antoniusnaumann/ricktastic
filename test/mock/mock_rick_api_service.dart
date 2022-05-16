e5b7bf21093998619166b891de4202c9c51d1666
Antonius Naumann <git@antonius.dev>
2022-05-16 14:50:48 +0200

    Test homescreen widgets

---
+++ test/mock/mock_rick_api_service.dart
@@ -0,0 +1,32 @@
+
+import 'dart:io';
+
+import 'package:ricktastic/src/entity/character.dart';
+import 'package:ricktastic/src/service/rick_api_service.dart';
+
+class MockRickApiService implements RickApiService {
+  @override
+  String get base => throw UnimplementedError();
+
+  @override
+  Stream<Page> fetchCharacters() async* {
+    for (final page in mockCharacterPages) {
+      yield page;
+      // sleep(const Duration(milliseconds: 50));
+    }
+  }
+}
+
+final mockCharacterPages = List<Page>.generate(50, (page) {
+  const charsPerPage = 100;
+  return Page(totalItems: charsPerPage * 50, content: List<Character>.generate(charsPerPage, (index) => Character(
+    id: _idFromPage(page, index, charsPerPage),
+    name: 'Bototron X${_idFromPage(page, index, charsPerPage)}',
+    species: 'Testbot',
+    image: '',
+    url: '',
+    created: DateTime.now().toIso8601String(),
+  )));
+});
+
+int _idFromPage(int page, int index, int charsPerPage) => charsPerPage * page + index + 1;
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,58 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ricktastic/src/app.dart';
+import 'package:ricktastic/src/bloc/characters_cubit.dart';
+import 'package:ricktastic/src/screen/home_screen.dart';
+import 'package:ricktastic/src/theme.dart';
+
+import 'mock/mock_rick_api_service.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const RicktasticApp());
+  testWidgets('Find navigation title', (WidgetTester tester) async {
+    await tester.pumpWidget(setupApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    if (kIsWeb) {
+      expect(find.text('Ricktastic'), findsNothing);
+    } else {
+      expect(find.text('Ricktastic'), findsOneWidget);
+    }
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  testWidgets('Verify characters are loaded', (WidgetTester tester) async {
+    await tester.pumpWidget(setupApp());
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.pump(const Duration(milliseconds: 10));
+
+    expect(find.text('Bototron X1'), findsOneWidget);
+    expect(find.text('Testbot'), findsWidgets);
+  });
+
+  testWidgets('Tap episodes tab and ensure content changes', (WidgetTester tester) async {
+    await tester.pumpWidget(setupApp());
+
+    expect(find.text('Coming Soon!'), findsNothing);
+    expect(find.text('Episodes'), findsOneWidget);
+    expect(find.byIcon(Icons.tv), findsOneWidget);
+    
+    await tester.tap(find.byIcon(Icons.tv));
+    await tester.pump(const Duration(milliseconds: 10));
+
+    expect(find.text('Coming Soon!'), findsOneWidget);
   });
 }
+
+Widget setupApp() => MaterialApp(
+  title: 'Ricktastic',
+  theme: lightTheme,
+  darkTheme: darkTheme,
+  themeMode: ThemeMode.system, 
+  home: BlocProvider(
+    create: (_) => CharactersCubit(MockRickApiService()),
+    child: const HomeScreen(title: 'Ricktastic'),
+  ),
+);
